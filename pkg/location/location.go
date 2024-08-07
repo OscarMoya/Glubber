85f7e734e2839c259d74f8263c8ebf38c14e3575
Oscar Moya <oscar.moya@alefedge.com>
2024-08-07 18:14:28 +0200

    Add missing doc to LocationManager interface
    Although minimal, some documentation is better than no-documentation
    
    Signed-off-by: Oscar Moya <oscar.moya@alefedge.com>

--- pkg/location/location.go
+++ pkg/location/location.go
@@ -4,6 +4,11 @@ import (
 	"context"
 )
 
+// LocationManager is an interface that defines the methods that a location manager should implementge
+// For now there are 3 methods: SaveDriverLocation, RemoveDriverLocation and GetNearbyDrivers
+// SaveDriverLocation saves the location of a driver given its ID and coordinates
+// RemoveDriverLocation removes the location of a driver given its ID
+// GetNearbyDrivers returns the IDs of the drivers that are near a given location and a radius in kilometers
 type LocationManager interface {
 	SaveDriverLocation(ctx context.Context, driverID string, latitude, longitude float64) error
 	RemoveDriverLocation(ctx context.Context, driverID string) error
