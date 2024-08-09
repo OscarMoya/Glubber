e908f03e0d145adb3188cb338ff98109eb7ceb23
Oscar Moya <o.moya.gomez@gmail.com>
2024-08-09 14:40:39 +0200

    WIP

---
+++ pkg/model/ride.go
@@ -0,0 +1,5 @@
+package model
+
+// Ride represents a ride in the system
+// Rides are created by passengers and are the main object that will contain the workflow to match a driver with a passenger
+// and to calculate the price of the ride-
